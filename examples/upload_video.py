926f4520480af535ccf72984717143e12ea16297
Erikka <19295862+erikkai@users.noreply.github.com>
2021-05-12 17:51:57 -0700

    Upload a video sample

---
+++ examples/upload_video.py
@@ -0,0 +1,38 @@
+import apivideo
+from apivideo.apis import VideosApi
+from apivideo.exceptions import ApiAuthException
+
+api_key = "your api key here"
+
+# Set up the authenticated client
+client = apivideo.AuthenticatedApiClient(api_key)
+
+# if you rather like to use the sandbox environment:
+# client = apivideo.AuthenticatedApiClient(api_key, production=False)
+client.connect()
+
+# Set up to use VideosApi 
+videos_api = VideosApi(client)
+
+# Create the payload with video details 
+video_create_payload = {
+    "title": "Client Video Test",
+    "description": "Client test",
+    "public": True,
+    "tags": ["bunny"]
+}
+
+# Create the container for your video and print the response
+response = videos_api.create(video_create_payload)
+print("Video Container", response)
+
+# Retrieve the video ID, you can upload once to a video ID
+video_id = response["video_id"]
+
+# Prepare the file you want to upload. Place the file in the same folder as your code.
+file = open("sample-mov-file.mov", "rb")
+
+# Upload your video. This handles videos of any size. If you want to upload from a link online, you need to u
+video_response = videos_api.upload(video_id, file)
+
+print("Uploaded Video", video_response)
